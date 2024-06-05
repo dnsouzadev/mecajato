deef26981e33f9da2097800fb6199a250f2b48ca
Daniel Souza <workdndsza@gmail.com>
2024-06-05 18:37:25 -0300

    colocando time  na mensagem de erro e criando funcao para a troca entre cadastro e atualizacao de cliente

--- templates/static/clientes/js/clientes.js
+++ templates/static/clientes/js/clientes.js
@@ -6,3 +6,31 @@ function add_carro() {
 
     container.innerHTML += html;
 }
+
+
+function exibir_form(tipo) {
+    const add_cliente = document.getElementById("add_cliente");
+    const att_cliente = document.getElementById("att_cliente");
+
+    if(tipo == "1"){
+        console.log(tipo, add_cliente, att_cliente);
+        att_cliente.style.display = "none";
+        add_cliente.style.display = "block";
+    } else if(tipo == "2"){
+        console.log(tipo, add_cliente, att_cliente);
+        add_cliente.style.display = "none";
+        att_cliente.style.display = "block";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Seleciona o elemento de erro
+    var erroMessage = document.getElementById("erro-message");
+
+    // Se o elemento existir, define um timer para ocultá-lo após 5 segundos
+    if (erroMessage) {
+        setTimeout(function() {
+            erroMessage.style.display = 'none';
+        }, 2000);
+    }
+});

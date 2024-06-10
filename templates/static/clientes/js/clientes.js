083631e233eb6a0821305810f5b4df07401906db
Daniel Souza <workdndsza@gmail.com>
2024-06-10 09:37:59 -0300

    atualizando os clientes e os carros e tbm poodendo excluir o carro

--- templates/static/clientes/js/clientes.js
+++ templates/static/clientes/js/clientes.js
@@ -1,64 +1,128 @@
-function add_carro() {
-    const container = document.getElementById("form-carro");
+function add_carro(){
+    const container = document.getElementById('form-carro')
 
-    const html = "<br><div class='row'> <div class='col-md'> <input type='text' placeholder='carro' class='form-control' name='carro'></div> <div class='col-md'><input type='text' placeholder='Placa' class='form-control' name='placa'> </div> <div class='col-md'><input type='number' placeholder='Ano' class='form-control' name='ano'> </div></div>"
+    const html = "<br>  <div class='row'> <div class='col-md'> <input type='text' placeholder='carro' class='form-control' name='carro' > </div> <div class='col-md'><input type='text' placeholder='Placa' class='form-control' name='placa' ></div> <div class='col-md'> <input type='number' placeholder='ano' class='form-control' name='ano'> </div> </div>"
 
-
-    container.innerHTML += html;
+    container.innerHTML += html
 }
 
+function exibir_form(tipo){
 
-function exibir_form(tipo) {
-    const add_cliente = document.getElementById("add_cliente");
-    const att_cliente = document.getElementById("att_cliente");
+    const add_cliente = document.getElementById('add_cliente')
+    const att_cliente = document.getElementById('att_cliente')
 
     if(tipo == "1"){
-        att_cliente.style.display = "none";
-        add_cliente.style.display = "block";
-    } else if(tipo == "2"){
+        att_cliente.style.display = "none"
+        add_cliente.style.display = "block"
+
+    }else if(tipo == "2"){
         add_cliente.style.display = "none";
-        att_cliente.style.display = "block";
+        att_cliente.style.display = "block"
     }
+
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    const erroMessage = document.getElementById("erro-message");
 
-    if (erroMessage) {
-        setTimeout(function() {
-            erroMessage.style.display = 'none';
-        }, 2000);
-    }
-});
+function dados_cliente(){
+    const cliente = document.getElementById('cliente-select')
+    const csrf_token = document.querySelector('[name=csrfmiddlewaretoken]').value
+    const id_cliente = cliente.value
 
-function dados_cliente() {
-    const cliente = document.getElementById("cliente-select");
-    const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
-    const url = '/clientes/atualiza_cliente/';
-    const id_cliente = cliente.value;
+    const data = new FormData()
+    data.append('id_cliente', id_cliente)
 
-    const data = new FormData();
-    data.append('id_cliente', id_cliente);
+    fetch("/clientes/atualiza_cliente/",{
+        method: "POST",
+        headers: {
+            'X-CSRFToken': csrf_token,
+        },
+        body: data
 
-    fetch(url, {
+    }).then(function(result){
+        return result.json()
+    }).then(function(data){
+        document.getElementById('form_att_cliente').style.display = 'block'
+        console.log(data['cliente'])
+
+        id = document.getElementById('id')
+        id.value = data['cliente_id']
+
+        nome = document.getElementById('nome')
+        nome.value = data['cliente']['nome']
+
+        sobrenome = document.getElementById('sobrenome')
+        sobrenome.value = data['cliente']['sobrenome']
+
+        cpf = document.getElementById('cpf')
+        cpf.value = data['cliente']['cpf']
+
+        email = document.getElementById('email')
+        email.value = data['cliente']['email']
+
+        div_carros = document.getElementById('carros')
+
+        for(i=0; i<data['carros'].length; i++){
+            div_carros.innerHTML += "\<form action='/clientes/update_carro/" + data['carros'][i]['id'] +"' method='POST'>\
+                <div class='row'>\
+                        <div class='col-md'>\
+                            <input class='form-control' name='carro' type='text' value='" + data['carros'][i]['fields']['carro'] + "'>\
+                        </div>\
+                        <div class='col-md'>\
+                            <input class='form-control' name='placa' type='text' value='" + data['carros'][i]['fields']['placa'] + "'>\
+                        </div>\
+                        <div class='col-md'>\
+                            <input class='form-control' type='text' name='ano' value='" + data['carros'][i]['fields']['ano'] + "' >\
+                        </div>\
+                        <div class='col-md'>\
+                            <input class='btn btn-lg btn-success' type='submit' value='Salvar'>\
+                        </div>\
+                    </form>\
+                    <div class='col-md'>\
+                        <a href='/clientes/excluir_carro/"+ data['carros'][i]['id'] +"' class='btn btn-lg btn-danger'>EXCLUIR</a>\
+                    </div>\
+                </div><br>"
+        }
+
+    })
+
+
+}
+
+
+function update_cliente(){
+    nome = document.getElementById('nome').value
+    sobrenome = document.getElementById('sobrenome').value
+    email = document.getElementById('email').value
+    cpf = document.getElementById('cpf').value
+    id = document.getElementById('id').value
+    const csrf_token = document.querySelector('[name=csrfmiddlewaretoken]').value
+
+    fetch('/clientes/update_cliente/' + id, {
         method: 'POST',
         headers: {
-            'X-CSRFToken': csrftoken
-    },
-    body: data
+            'X-CSRFToken': csrf_token,
+        },
+        body: JSON.stringify({
+            nome: nome,
+            sobrenome: sobrenome,
+            email: email,
+            cpf: cpf,
+        })
+
+    }).then(function(result){
+        return result.json()
+    }).then(function(data){
+
+        if(data['status'] == '200'){
+            nome = data['nome']
+            sobrenome = data['sobrenome']
+            email = data['email']
+            cpf = data['cpf']
+            console.log('Dados alterado com sucesso')
+        }else{
+            console.log('Ocorreu algum erro')
+        }
+
     })
-    .then(response => response.json())
-    .then(data => {
-        const nome = document.getElementById("nome");
-        const sobrenome = document.getElementById("sobrenome");
-        const email = document.getElementById("email");
-        const cpf = document.getElementById("cpf");
 
-        form_att_cliente.style.display = "block";
-
-        nome.value = data.nome;
-        sobrenome.value = data.sobrenome;
-        email.value = data.email;
-        cpf.value = data.cpf;
-    });
 }

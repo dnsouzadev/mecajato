6a700be501cf8a17474ca59c3f733e1f8eb97845
Daniel Souza <workdndsza@gmail.com>
2024-06-11 12:44:22 -0300

    listando os servicos

--- servicos/views.py
+++ servicos/views.py
@@ -1,12 +1,10 @@
 from django.http import HttpResponseRedirect, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from servicos.models import Servico
 from .forms import FormServico
 
 # Create your views here.
-def servicos(request):
-    return JsonResponse({"servicos": "servicos"})
-
-
 def novo_servico(request):
     if request.method == 'GET':
         form = FormServico()
@@ -20,3 +18,22 @@ def novo_servico(request):
             return render(request, "novo_servico.html", {"form": form})
     else:
         return render(request, "novo_servico.html", {"form": FormServico()})
+
+
+def listar_servico(request):
+    if request.method == 'GET':
+        servico = Servico.objects.all()
+        return render(request, "listar_servico.html", {"servicos": servico})
+
+
+def servico(request, identificador):
+    if request.method == 'GET':
+        servico = get_object_or_404(Servico, identificador=identificador)
+        return render(request, "servico.html", {"servico": servico})
+    elif request.method == 'POST':
+        servico = Servico.objects.get(identificador=identificador)
+        servico.finalizado = True
+        servico.save()
+        return HttpResponseRedirect("/listar_servico/")
+    else:
+        return render(request, "servico.html", {"servico": Servico.objects.get(identificador=identificador)})

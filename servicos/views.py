7150f905ddb3779ded4747f93a405064e14fac9e
Daniel Souza <workdndsza@gmail.com>
2024-06-11 13:16:20 -0300

    adicionadno o geramento de os

--- servicos/views.py
+++ servicos/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponseRedirect, JsonResponse
+from io import BytesIO
+from django.http import FileResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import get_object_or_404, render
-
+from fpdf import FPDF
 from servicos.models import Servico
 from .forms import FormServico
 
@@ -34,6 +35,39 @@ def servico(request, identificador):
         servico = Servico.objects.get(identificador=identificador)
         servico.finalizado = True
         servico.save()
-        return HttpResponseRedirect("/listar_servico/")
+        return HttpResponseRedirect("servico/listar_servico/")
     else:
         return render(request, "servico.html", {"servico": Servico.objects.get(identificador=identificador)})
+
+
+def gerar_os(request, identificador):
+    servico = get_object_or_404(Servico, identificador=identificador)
+
+    pdf = FPDF()
+    pdf.add_page()
+
+    pdf.set_font('Arial', 'B', 12)
+
+    pdf.set_fill_color(240,240,240)
+    pdf.cell(35, 10, 'Cliente:', 1, 0, 'L', 1)
+    pdf.cell(0, 10, f'{servico.cliente.nome}', 1, 1, 'L', 1)
+
+    pdf.cell(35, 10, 'Manutenções:', 1, 0, 'L', 1)
+
+    categorias_manutencao = servico.categoria_manutencao.all()
+    for i, manutencao in enumerate(categorias_manutencao):
+        pdf.cell(0, 10, f'- {manutencao.get_titulo_display()}', 1, 1, 'L', 1)
+        if not i == len(categorias_manutencao) -1:
+            pdf.cell(35, 10, '', 0, 0)
+
+    pdf.cell(35, 10, 'Data de início:', 1, 0, 'L', 1)
+    pdf.cell(0, 10, f'{servico.data_inicio}', 1, 1, 'L', 1)
+    pdf.cell(35, 10, 'Data de entrega:', 1, 0, 'L', 1)
+    pdf.cell(0, 10, f'{servico.data_entrega}', 1, 1, 'L', 1)
+    pdf.cell(35, 10, 'Protocolo:', 1, 0, 'L', 1)
+    pdf.cell(0, 10, f'{servico.protocolo}', 1, 1, 'L', 1)
+
+    pdf_content = pdf.output(dest='S').encode('latin-1')
+    pdf_bytes = BytesIO(pdf_content)
+
+    return FileResponse(pdf_bytes, as_attachment=True, filename=f"os-{servico.protocolo}.pdf")
